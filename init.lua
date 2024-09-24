b9a8c814f618b1a9f52f970a85506516c5055a9c
kazuki-oshino <kazuki.oshino@showcase-gig.com>
2024-09-24 13:27:05 +0900

    yank color

--- init.lua
+++ init.lua
@@ -9,3 +9,11 @@ vim.api.nvim_create_autocmd("BufWritePost", {
     pattern = { "plugins.lua" },
     command = "PackerCompile",
 })
+
+vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#FF1493" })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ higroup = 'YankHighlight', timeout = 200 })
+  end,
+})
