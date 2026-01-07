37f035830e01f79466898640fdd915521a9b46c3
kazuki-oshino <slash0juice@gmail.com>
2026-01-07 15:22:23 +0900

    chore: NvimTreeプラグインの設定を更新。

--- lua/nvimtree_config.lua
+++ lua/nvimtree_config.lua
@@ -8,7 +8,51 @@ end
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- カスタム関数: パスをクリップボードにコピー
+local api = require('nvim-tree.api')
+
+local function copy_absolute_path()
+    local node = api.tree.get_node_under_cursor()
+    if node then
+        vim.fn.setreg('+', node.absolute_path)
+        vim.notify('コピーしました: ' .. node.absolute_path)
+    end
+end
+
+local function copy_relative_path()
+    local node = api.tree.get_node_under_cursor()
+    if node then
+        local relative_path = vim.fn.fnamemodify(node.absolute_path, ':.')
+        vim.fn.setreg('+', relative_path)
+        vim.notify('コピーしました: ' .. relative_path)
+    end
+end
+
+local function copy_filename()
+    local node = api.tree.get_node_under_cursor()
+    if node then
+        vim.fn.setreg('+', node.name)
+        vim.notify('コピーしました: ' .. node.name)
+    end
+end
+
+-- nvim-tree用のキーマッピング設定
+local function on_attach(bufnr)
+    local function opts(desc)
+        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    -- デフォルトのキーマッピングを適用
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- カスタムキーマッピング追加
+    vim.keymap.set('n', 'Y', copy_absolute_path, opts('絶対パスをコピー'))
+    vim.keymap.set('n', 'y', copy_relative_path, opts('相対パスをコピー'))
+    vim.keymap.set('n', 'yn', copy_filename, opts('ファイル名をコピー'))
+end
+
 nvimtree.setup({
+    on_attach = on_attach,
     -- gitステータス表示
     git = {
         enable = true,
